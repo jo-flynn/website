d3bdb73691fd878c2fd15870000efd7152c7ead6
Joe Flynn <me@joeflynn.net>
2011-04-29 22:20:58 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '19f5c2a5c7a560d53ca9acbdcdff576fd41d63ce2138293a7fa59fffa1be812e6c9316708d14ddc5d75dfd80a3c09de8cf831c9408906b4c953afa0a2e4e9884'
---
+++ db/migrate/20110413021622_create_pieces.rb
@@ -0,0 +1,18 @@
+class CreatePieces < ActiveRecord::Migration
+  def self.up
+    create_table :works do |t|
+      t.string :name
+      t.date :created
+      t.string :preview_img_url
+      t.string :full_img_url
+      t.string :note
+      t.boolean :visible
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :works
+  end
+end
